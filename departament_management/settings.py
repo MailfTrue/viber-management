ee8861ba9f0785dbf2e391584b3381cb976cb7f2
mailf <mailf.dev@yandex.ru>
2020-06-11 17:05:40 +1000

    celery settings

--- departament_management/settings.py
+++ departament_management/settings.py
@@ -128,3 +128,11 @@ STATIC_ROOT = 'static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media/'
+
+
+# celery
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
